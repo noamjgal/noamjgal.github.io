3132ededf03785a868d852e0831323e5080dda99
noamjgal <noamjgal@gmail.com>
2025-01-21 12:56:30 +0200

    created custom readme and fixed basePath in next.config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/{repo-name}' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio_website' : '',
   images: {
     unoptimized: true,
   },
