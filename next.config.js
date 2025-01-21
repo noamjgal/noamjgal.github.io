bf38894ae9bda3746a2e92de670c5309b364cc91
noamjgal <noamjgal@gmail.com>
2025-01-21 16:04:52 +0200

    adjusted to match new domain

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  module.exports = nextConfig;
+  output: 'export',
+  basePath: '',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
